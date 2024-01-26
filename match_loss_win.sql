259f9f00e160be77dccba970c1ccd9f0dba8ed57
Chandan Soni <90852214+Chandansoni2021@users.noreply.github.com>
2024-01-26 12:29:28 +0530

    Add files via upload

---
+++ match_loss_win.sql
@@ -0,0 +1,25 @@
+use match;
+create table icc_world_cup
+(
+Team_1 Varchar(20),
+Team_2 Varchar(20),
+Winner Varchar(20)
+);
+INSERT INTO icc_world_cup values('India','SL','India');
+INSERT INTO icc_world_cup values('SL','Aus','Aus');
+INSERT INTO icc_world_cup values('SA','Eng','Eng');
+INSERT INTO icc_world_cup values('Eng','NZ','NZ');
+INSERT INTO icc_world_cup values('Aus','India','India');
+
+select * from icc_world_cup;
+
+select Team_1 as team_name,count(1) as no_of_matches_plaes, sum(win_flag) as no_of_won, count(1)-sum(win_flag) as no_of_loss from(
+select Team_1,case when Team_1=winner then 1 else 0 end as win_flag
+from icc_world_cup
+union all
+select Team_2,case when Team_2=winner then 1 else 0 end as win_flag
+from icc_world_cup) A 
+group by Team_1
+order by no_of_won desc 
+
+
